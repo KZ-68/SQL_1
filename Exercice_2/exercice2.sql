e001d6c05bbc211497c64a0a141db89ccebffde9
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-05-25 12:17:07 +0200

    fix: change AVG function

--- Exercice_2/exercice2.sql
+++ Exercice_2/exercice2.sql
@@ -11,8 +11,10 @@ FROM etudiant r
 
 -- b) Quelles sont, parmi l'ensemble des notes, la note la plus haute et la note la plus basse ?
 
-SELECT MIN(r.note), MAX(r.note)
+SELECT r.numero_etudiant_id, MIN(r.note) as note_min, MAX(r.note) as note_max
 FROM evaluer r
+GROUP BY r.numero_etudiant_id
+-- GROUPE BY contient tous les champs présents dans SELECT à l'exception des fonctions d'agrégations.
 
 -- c) Quelles sont les moyennes de chaque étudiant dans chacune des matières ? (utilisez CREATE VIEW)
 
@@ -20,14 +22,14 @@ FROM evaluer r
 CREATE VIEW moyennesEtudiant AS
 -- Lecture et sélection des colonnes
 SELECT r.nom, 
-r.prenom, 
-s.libellemat, 
-AVG(t.note * s.coeffmat / s.coeffmat) AS moyennesGeneraleEtudiants -- On donne un nom personnalisée à cette sélection
+    r.prenom, 
+    s.libellemat, 
+AVG(t.note) AS moyennesMatiere -- On donne un nom personnalisée à cette sélection
 -- On spécifie quelle(s) table(s) peut être sélectionnée(s) 
 FROM etudiant r, evaluer t, matiere s
 -- Fait la jointure des tables
 WHERE r.numero_etudiant = t.numero_etudiant_id AND s.codemat = t.code_mat
-GROUP BY r.numero_etudiant, r.nom, r.prenom, s.libellemat;
+GROUP BY r.nom, r.prenom, s.libellemat;
 
 -- d) Quelles sont les moyennes par matière ? (cf. question c)
 
