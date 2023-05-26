10d0d3e5105e8a364fab12d0b5c2eea8f77cb2f8
Kevin ZITNIK <zitnik.kevin@gmail.com>
2023-05-26 09:17:49 +0200

    feat: add new file, not completed

---
+++ Exercice_3/exercice3.sql
@@ -0,0 +1,111 @@
+-- 1) Soit le schéma relationnel suivant :
+-- ARTICLES(NOART, LIBELLE, STOCK, PRIXINVENT)
+-- FOURNISSEURS(NOFOUR, NOMFOUR, ADRFOUR, VILLEFOUR)
+-- ACHETER(NOFOUR#, NOART#, PRIXACHAT, DELAI)
+-- Affichez les résultats suivants avec une solution SQL:
+
+-- a) Numéros et libellés des articles dont le stock est inférieur à 10 ?
+
+SELECT a.noart, a.libelle, a.stock
+FROM articles a
+WHERE a.stock < 10
+
+-- b) Quelles sont, parmi l'ensemble des notes, la note la plus haute et la note la plus basse ?
+
+SELECT a.noart, a.libelle, a.prixinvent
+FROM articles a
+WHERE a.prixinvent BETWEEN 100 AND 300
+
+-- c) Liste des fournisseurs dont on ne connaît pas l'adresse ?
+
+SELECT f.nofour, f.nomfour, f.adrfour, f.villefour
+FROM fournisseurs f
+WHERE f.adrfour IS NULL
+
+-- d) Liste des fournisseurs dont le nom commence par "STE" ?
+
+SELECT f.nofour, f.nomfour, f.adrfour, f.villefour
+FROM fournisseurs f
+WHERE f.nomfour LIKE "STE%"
+
+-- e) Noms  et  adresses  des  fournisseurs  qui  proposent  des  articles  pour  lesquels  le  délai d'approvisionnement est supérieur à 20 jours ?
+
+CREATE VIEW delaiFournisseurs AS
+SELECT 
+    f.nofour, 
+    f.nomfour, 
+    f.adrfour, 
+    f.villefour
+FROM fournisseurs f
+INNER JOIN 
+    acheter ach 
+    ON 
+    f.nofour = ach.nofour_id
+WHERE ach.delai>20
+GROUP BY 
+    f.nofour, 
+    f.nomfour, 
+    f.adrfour, 
+    f.villefour
+
+-- f) Nombre d'articles référencés ?
+
+SELECT COUNT(a.noart)
+FROM articles a
+WHERE 
+    a.libelle IS NOT NULL
+    AND 
+        a.stock IS NOT NULL
+    AND 
+        a.prixinvent IS NOT NULL
+
+-- g) Valeur du stock ?
+
+SELECT ROUND(SUM(a.prixinvent), 2)
+FROM articles a
+
+-- h) Numéros et libellés des articles triés dans l'ordre décroissant des stocks ?
+
+SELECT 
+    a.noart, 
+    a.libelle
+FROM 
+    articles a
+ORDER BY 
+    a.stock DESC
+
+-- i) Liste pour chaque article (numéro et libellé) du prix d'achat maximum, minimum et moyen ?
+
+SELECT 
+    a.noart,
+    a.libelle,
+    MIN(ach.prixachat) AS prix_min, 
+    MAX(ach.prixachat) AS prix_max,
+    AVG(ach.prixachat) AS prix_moyen
+FROM
+    articles a
+INNER JOIN acheter ach ON a.noart = ach.noart_id
+GROUP BY a.noart, a.libelle
+
+-- j) Délai moyen pour chaque fournisseur proposant au moins 2 articles ?
+
+CREATE OR REPLACE VIEW delaiarticlesfournisseurs AS
+SELECT 
+    COUNT(articles) AS nb_art,
+    a.libelle,
+    f.nofour, 
+    f.nomfour, 
+    f.adrfour, 
+    f.villefour, 
+    AVG(ach.delai) AS moyenneDelai
+FROM 
+	acheter ach
+INNER JOIN fournisseurs f ON f.nofour = ach.nofour_id  
+INNER JOIN articles a ON a.noart = ach.noart_id
+HAVING nb_art > 2
+GROUP BY 
+    a.libelle,
+    f.nofour, 
+    f.nomfour, 
+    f.adrfour, 
+    f.villefour
